3c6a66f6d77e30da8e206c7f3a5e167936de4c37
JordanAguiriano <JordanAguiriano@hotmail.com>
2020-01-06 21:30:25 +0000

    add value working

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [value, setValue] = useState();
+	const [toDo, setToDo] = useState([]);
+
+	function addToDO(e) {
+		if (value === "") return;
+		setToDo([...toDo, { text: value }]);
+		setValue("");
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="text-center mt-5">
+				<h1>To Do List</h1>
+				<input
+					id="input"
+					type="text"
+					value={value}
+					onChange={e => setValue(e.target.value)}
+				/>
+				<button onClick={addToDO}>Add</button>
+				<ul>
+					{toDo.map(todo => (
+						<li id="list" key={todo.index}>
+							{todo.text}
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
 	);
 }
